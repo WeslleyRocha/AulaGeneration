75c5390d0b6a1d27d6eba6024882fcdaf2b1251a
Weslley Rocha <weslley.nx@gmail.com>
2020-09-17 15:07:41 -0300

    Atualizando

--- Generation/src/Herança_Polimorfismo/Animal_Classe_Mãe.java
+++ Generation/src/Herança_Polimorfismo/Animal_Classe_Mãe.java
@@ -2,16 +2,17 @@ package Herança_Polimorfismo;
 
 public class Animal_Classe_Mãe {
 
+	/*  Exercícios Herança/Polimorfismo
+
+1- Crie uma hierarquia de classes conforme abaixo com os seguintes atributos e comportamentos (observe a tabela), utilize os seus 
+conhecimentos e distribua as características de forma que tudo o que for comum a todos os animais fique na classe Animal:
+
+2- Implemente um programa que crie os 3 tipos de animais definidos no exercício anterior e invoque o método que emite o som de 
+cada um de forma polimórfica, isto é, independente do tipo de animal. */
+	
 	private String nome;
 	private int idade;
 	
-	/*public Animal_Classe_Mãe (String nome, String som, int idade)
-	{	
-		this.nome=nome;
-		this.som=som;
-		this.idade=idade;		
-	}*/
-
 	public String getNome() {
 		return nome;
 	}
