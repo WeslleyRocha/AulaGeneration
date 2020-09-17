792899ef0dbcd2c2f8fe010275ead0818da1d23c
Weslley Rocha <weslley.nx@gmail.com>
2020-09-16 23:17:47 -0300

    Atualizando Arquivos

---
+++ Generation/src/Heranças/Teste_Ex06_16_09_20.java
@@ -0,0 +1,22 @@
+package Heranças;
+
+public class Teste_Ex06_16_09_20 {
+
+	public static void main(String[] args) {
+		
+		Ex06_16_09_20 pessoa = new Ex06_16_09_20 ("Franco", "LaFenina", "123456", "123456", 20 , 5.60, 20);
+		Ex06_16_09_20 pessoa1 = new Ex06_16_09_20 ("Matheus", "Belo", "45689656", "123456", 11 , 4.60, 20);
+		Ex06_16_09_20 pessoa2 = new Ex06_16_09_20 ("Alex", "Menino", "546786", "123456", 15.99 , 7.60, 20 );
+		Ex06_16_09_20 pessoa3 = new Ex06_16_09_20 ("Weslley", "Rocha", "6465456", "123456", 78.80 , 7.90 , 20);
+		Ex06_16_09_20 pessoa4 = new Ex06_16_09_20 ("Vanessa", "Pereira", "6454656", "123456", 90.77 , 8.00 , 20);
+	
+		pessoa.ValoresVendas();
+		pessoa1.ValoresVendas();
+		pessoa2.ValoresVendas();
+		pessoa3.ValoresVendas();
+		pessoa4.ValoresVendas();
+		
+		
+	}
+
+}
